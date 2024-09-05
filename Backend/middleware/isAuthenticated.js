ba451e10ea92b1f80ede1e15e332df6d0bad96f9
Bhavishya Tomer <bhavishya.tomer@dxc.com>
2024-09-05 22:42:28 +0530

    company creation

--- Backend/middleware/isAuthenticated.js
+++ Backend/middleware/isAuthenticated.js
@@ -23,6 +23,7 @@ const isAuthenticated = async (req, res, next) => {
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: "Internal Server Error", success: false });
+        
     }
 };
 
